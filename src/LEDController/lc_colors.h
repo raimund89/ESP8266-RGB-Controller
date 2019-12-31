38cb89ffa4875f2336fb34e55119f8e4d357b126
raimund89 <raimund89@users.noreply.github.com>
2019-12-31 01:37:40 +0100

    Extended color struct
    To include the brightness channel

--- src/LEDController/lc_colors.h
+++ src/LEDController/lc_colors.h
@@ -1,18 +1,24 @@
 #ifndef LC_COLORS
 #define LC_COLORS
 
+// r - red
+// g - green
+// b - blue
+// l - lightness/brightness
+// w - warm white
 typedef struct {
     byte r;
     byte g;
     byte b;
+    byte l;
     byte w;
 } Color;
 
 // Some default colors
-#define COLOR_BLACK {  0,   0,   0,   0}
-#define COLOR_RED   {255,   0,   0,   0}
-#define COLOR_GREEN {  0, 255,   0,   0}
-#define COLOR_BLUE  {  0,   0, 255,   0}
-#define COLOR_WHITE {  0,   0,   0, 255}
+#define COLOR_BLACK {  0,   0,   0, 128,   0}
+#define COLOR_RED   {255,   0,   0, 128,   0}
+#define COLOR_GREEN {  0, 255,   0, 128,   0}
+#define COLOR_BLUE  {  0,   0, 255, 128,   0}
+#define COLOR_WHITE {  0,   0,   0, 128, 255}
 
 #endif
