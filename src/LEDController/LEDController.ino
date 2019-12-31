df571b2f1b12a599e82b6d0953818c9a432071d4
raimund89 <raimund89@users.noreply.github.com>
2019-12-31 01:38:06 +0100

    Include lc_settings in main code
    Missed that

--- src/LEDController/LEDController.ino
+++ src/LEDController/LEDController.ino
@@ -1,10 +1,13 @@
 #include "lc_serial.h"
+#include "lc_settings.h"
 #include "lc_colors.h"
 #include "lc_gpio.h"
 #include "lc_wifi.h"
 #include "lc_ota.h"
 #include "lc_webserver.h"
 #include "lc_programs.h"
+
+// Very important part to do: don't read color from the analogRead. Save the current color, then use that to set the current color. Can use analogRead to confirm correct writing...
 
 void setup() {
   InitSerial();
