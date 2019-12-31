fde85ce092a02686ac5ac28c4c432497ca563bf5
raimund89 <raimund89@users.noreply.github.com>
2019-12-31 01:38:31 +0100

    Moved programs back to the GPIO
    For now, so it compiles. Need to change this back, but later.

--- src/LEDController/lc_gpio.h
+++ src/LEDController/lc_gpio.h
@@ -12,6 +12,38 @@
 
 void SetColor(Color c);
 Color GetColor();
+unsigned long GetColorHex();
+
+#include <Ticker.h>
+
+Ticker program_ticker;
+
+void Tick_CycleColors() {
+  switch(GetColorHex()){
+    case 0xFF000000L:
+      SetColor(COLOR_GREEN);
+      break;
+    case 0x00FF0000L:
+      SetColor(COLOR_BLUE);
+      break;
+    case 0x0000FF00L:
+      SetColor(COLOR_RED);
+      break;
+    default:
+      // This will be called the first time, when the
+      // LEDs are still off after GPIO initialization
+      SetColor(COLOR_RED);
+      break;
+  }
+}
+
+void Program_CycleColors(float speed) {
+  program_ticker.attach(speed, Tick_CycleColors);
+}
+
+void Program_Clear() {
+  program_ticker.detach();
+}
 
 void InitGPIO()
 {
